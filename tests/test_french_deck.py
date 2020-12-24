a9d409097cb3805521e2602d6ea2de2a281e2e3d
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-24 01:46:40 -0500

    Added a test folder

---
+++ tests/test_french_deck.py
@@ -0,0 +1,17 @@
+import unittest
+
+from deck_game import FrenchDeck
+
+class TestFrenchDeck(unittest.TestCase):
+    
+    def test_init_cards(self):
+        """
+        Test that the deck contains 52 cards
+        """
+        data = 52
+        deck = FrenchDeck()
+        self.assertEqual(len(deck.cards), 52)
+        
+
+if __name__ == '__main__':
+    unittest.main()
