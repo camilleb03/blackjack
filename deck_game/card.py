e76e5feeb428828ab2ade37481544d20c3899eb9
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-24 01:44:45 -0500

    Removed constants file containing ranks and suits

--- deck_game/card.py
+++ deck_game/card.py
@@ -1,15 +1,20 @@
-from deck_game.constants import SUITS, RANKS
-
 class Card:
     """
     Represent a single playing card
     """
-    def __init__(self, rank, suit):
+    def __init__(self, rank, suit, hidden=True):
         self.rank = rank
         self.suit = suit
+        self.hidden = hidden
+
+    def flip(self):
+        self.hidden = not self.hidden
     
     def __str__(self):
-        return '%s of %s' % (RANKS[self.rank],SUITS[self.suit])
+        if self.hidden:
+            return 'Unknown'
+        else:
+            return '%s of %s' % (self.rank,self.suit)
 
     def __eq__(self, other):
         return ((self.suit, self.rank) == (other.suit, other.rank))
--- deck_game/constants.py
+++
@@ -1,3 +0,0 @@
-SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
-RANKS = [None, 'Ace', '2', '3', '4', '5', '6', '7',
-            '8', '9', '10', 'Jack', 'Queen', 'King']
