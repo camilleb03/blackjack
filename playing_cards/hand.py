020b2715305e4660861c9a77f576f96283e60660
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-25 17:11:08 -0500

    Removed inheritance between Hand and Deck classes

--- playing_cards/hand.py
+++ playing_cards/hand.py
@@ -1,13 +1,24 @@
-from .deck import Deck
-
-class Hand(Deck):
+class Hand():
     """
     Represents a hand of cards
     """
     def __init__(self, player_name):
         self.cards = []
         self.player_name = player_name
+    
+    def sort(self, order=False):
+        self.cards.sort(reverse=order)
+
+    def remove_card(self, card):
+        if self.check_enough_cards_in_deck(1):
+            self.cards.remove(card)
+
+    def add_card(self, card):
+        self.cards.append(card)
 
     def __str__(self):
-        return 'Hand of %s: \n' % (self.player_name) + super(Hand, self).__str__()
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return 'Hand of %s: \n' % (self.player_name) + '\n'.join(res)
         
