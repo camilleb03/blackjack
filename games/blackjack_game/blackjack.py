7772f85798f6a985a41985dc854cd79e1d52e410
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-26 13:56:34 -0500

    New function to handle game statuses between player and dealer. Tie in particular

--- games/blackjack_game/blackjack.py
+++ games/blackjack_game/blackjack.py
@@ -1,5 +1,5 @@
 from .blackjack_hand import BlackJackHand
-from playing_cards import FrenchDeck
+from playing_cards import FrenchDeck, GameStatus
 
 class BlackJack():
     """
@@ -57,7 +57,8 @@ class BlackJack():
         print(hand)
         print(f"Value of {hand.value}")
         print("+----------------+")
-
+    """
+    Old function that does not manage TIE
     def find_winners(self):
         winners = []
         # Check who won between dealer and each player
@@ -67,7 +68,6 @@ class BlackJack():
                 print(f"Player {hand.player_name} and {self.dealer.player_name} \
                         busted with value {hand.value} and {self.dealer.value}.")
                 continue
-            # FIXME: Prints as many times as the number of players
             # Dealer busted, then player win
             if self.dealer.busted:
                 winners.append(hand)
@@ -77,19 +77,69 @@ class BlackJack():
             if hand.busted:
                 print(f"Player {hand.player_name} busted with value {hand.value}")
                 continue
-            # TODO: Manage tie between the two
             # Neither busted, compare hand value
             if hand.calculate_value() > self.dealer.calculate_value():
                 winners.append(hand)
 
         return winners
+    """
 
-    def show_game_winners(self, winners):
-        for player in self.hands:
-            if player in winners:
-                print(f"Player {player.player_name} won !")
+    def calculate_player_status(self):
+        players_status = []
+        # Check who won between dealer and each player
+        for hand in self.hands:
+            # Player busted, then dealer win (no matter if dealer busted or not)
+            if hand.busted:
+                players_status.append(tuple((hand, GameStatus.LOSE)))
+                print(f"Player {hand.player_name} busted with value {hand.value}")
+                continue
+            # Dealer busted, then player win (passed the busted check)
+            # FIXME: Prints as many times as the number of players
+            if self.dealer.busted:
+                players_status.append(tuple((hand, GameStatus.WIN)))
+                print(f"{self.dealer.player_name} busted with value {self.dealer.value}.")
+                continue
+            # Neither busted, compare hand value
+            # Player has higher score -> WIN
+            if hand.value > self.dealer.value:
+                players_status.append(tuple((hand, GameStatus.WIN)))
+            elif (hand.value == self.dealer.value):
+                # PLayer and Dealer has the same score -> =21
+                if (hand.value == 21):
+                    # Both have a blackjack -> TIE
+                    if (len(hand.cards) == 2 and (len(self.dealer.cards) == 2)):
+                        players_status.append(tuple((hand, GameStatus.TIE)))
+                        print(f"Player {hand.player_name} AND {self.dealer.player_name} have a Blackjack.")
+                    # Player has blackjack -> WIN
+                    elif (len(hand.cards) == 2):
+                        players_status.append(tuple((hand, GameStatus.WIN)))
+                        print(f"Player {hand.player_name} has a Blackjack.")
+                    # Dealer has blackjack -> LOSE
+                    elif (len(hand.cards) == 2):
+                        players_status.append(tuple((hand, GameStatus.LOSE)))
+                        print(f"{self.dealer.player_name} has a Blackjack.")
+                    # Player and Dealer do not have blackjack -> TIE
+                    else:
+                        players_status.append(tuple((hand, GameStatus.TIE)))
+                        print(f"Neither Player {hand.player_name} nor {self.dealer.player_name} has a Blackjack.")
+                # PLayer and Dealer has the same score -> <21
+                else:
+                    players_status.append(tuple((hand, GameStatus.TIE)))
+                    print(f"Player {hand.player_name} and {self.dealer.player_name} obtained {hand.value}.")
+            # Player has a value lower than Dealer -> LOSE
             else:
-                print(f"Player {player.player_name} lost !")
+                players_status.append(tuple((hand, GameStatus.LOSE)))
+
+        return players_status
+
+    def show_game_players_status(self, players_status):
+        for player_status in players_status:
+            if player_status[1] == GameStatus.WIN:
+                print(f"Player {player_status[0].player_name} won !")
+            if player_status[1] == GameStatus.TIE:
+                print(f"Player {player_status[0].player_name} tied !")
+            if player_status[1] == GameStatus.LOSE:
+                print(f"Player {player_status[0].player_name} lost !")
 
     def show_game_title(self):
         print("+--------------------------------+")
@@ -154,6 +204,6 @@ class BlackJack():
         # Show final table (all hands)
         self.show_table()
 
-        # Declare winners
-        winners = self.find_winners()
-        self.show_game_winners(winners)
+        # Find game status for each player
+        players_status = self.calculate_player_status()
+        self.show_game_players_status(players_status)
