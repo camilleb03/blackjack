95efde0cc11f7c640ff7243e8c3414cb3e4210bb
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 01:42:12 -0500

    Reformat code and modified removed_card() tests

--- tests/tests_playing_cards/test_french_deck.py
+++ tests/tests_playing_cards/test_french_deck.py
@@ -1,18 +1,20 @@
-import unittest
 import itertools
+import unittest
+
 from playing_cards import FrenchDeck, Card, Hand
+
 
 class TestFrenchDeck(unittest.TestCase):
 
     def setUp(self):
         self.deck = FrenchDeck()
-    
+
     def test_generate_one_deck_length(self):
         """
         Check that deck contains 52 cards
         """
         self.assertEqual(len(self.deck.cards), 52)
-    
+
     def test_generate_three_decks_length(self):
         """
         Check that deck contains 156 cards
@@ -29,13 +31,13 @@ class TestFrenchDeck(unittest.TestCase):
         self.deck.cards[0].suit = 'Hearts'
         self.assertNotEqual(self.deck.cards[0], self.deck.cards[52])
         self.assertIsNot(self.deck.cards[0], self.deck.cards[52])
-    
+
     def test_generate_cards_content(self):
         """
         Check that deck contains all combinations of ranks and suits
         """
         for idx, i in enumerate(itertools.product(self.deck.card_suits, self.deck.card_ranks)):
-            card = Card(i[1],i[0])
+            card = Card(i[1], i[0])
             self.assertEqual(self.deck.cards[idx].rank, card.rank)
             self.assertEqual(self.deck.cards[idx].suit, card.suit)
         # self.assertCountEqual(self.deck.cards, data)
@@ -49,8 +51,8 @@ class TestFrenchDeck(unittest.TestCase):
         data2 = Card('K', 'Clubs')
         self.assertEqual(self.deck.cards[0].rank, data.rank)
         self.assertEqual(self.deck.cards[0].suit, data.suit)
-        self.assertEqual(self.deck.cards[len(self.deck.cards)-1].rank, data2.rank)
-        self.assertEqual(self.deck.cards[len(self.deck.cards)-1].suit, data2.suit)
+        self.assertEqual(self.deck.cards[len(self.deck.cards) - 1].rank, data2.rank)
+        self.assertEqual(self.deck.cards[len(self.deck.cards) - 1].suit, data2.suit)
 
     # FIXME: Mock something to test better if by any chance the first or last card stay at the same position
     def test_shuffle_cards_in_deck(self):
@@ -60,10 +62,10 @@ class TestFrenchDeck(unittest.TestCase):
         data = Card('A', 'Spades')
         data2 = Card('K', 'Clubs')
         self.deck.shuffle()
-        self.assertTrue((self.deck.cards[0].rank != data.rank) or (self.deck.cards[0].suit !=data.suit))
-        self.assertTrue((self.deck.cards[len(self.deck.cards)-1].rank != data2.rank) or (self.deck.cards[len(self.deck.cards)-1].suit !=data2.suit))
+        self.assertTrue((self.deck.cards[0].rank != data.rank) or (self.deck.cards[0].suit != data.suit))
+        self.assertTrue((self.deck.cards[len(self.deck.cards) - 1].rank != data2.rank) or (
+                    self.deck.cards[len(self.deck.cards) - 1].suit != data2.suit))
 
-    
     # FIXME: Not working because of card comparison (lesser than)
     @unittest.skip("WIP : less_than not implemented yet")
     def test_sort_cards_in_deck(self):
@@ -76,8 +78,8 @@ class TestFrenchDeck(unittest.TestCase):
         self.deck.sort()
         self.assertEqual(self.deck.cards[0].rank, data.rank)
         self.assertEqual(self.deck.cards[0].suit, data.suit)
-        self.assertEqual(self.deck.cards[len(self.deck.cards)-1].rank, data2.rank)
-        self.assertEqual(self.deck.cards[len(self.deck.cards)-1].suit, data2.suit)
+        self.assertEqual(self.deck.cards[len(self.deck.cards) - 1].rank, data2.rank)
+        self.assertEqual(self.deck.cards[len(self.deck.cards) - 1].suit, data2.suit)
 
     def test_add_card(self):
         """
@@ -86,24 +88,25 @@ class TestFrenchDeck(unittest.TestCase):
         data = Card('8', 'Spades')
         self.deck.add_card(data)
         self.assertEqual(len(self.deck.cards), 53)
-        self.assertEqual(self.deck.cards[53-1], data)
+        self.assertEqual(self.deck.cards[53 - 1], data)
 
-    def test_remove_card_valid(self):
+    def test_remove_card_by_value_valid(self):
         """
-        Check card is not in the deck after removing it
+        Check card is not in the deck after removing it and that the right card is removed
         """
         data = Card('8', 'Spades')
-        self.deck.remove_card(data)
+        removed_card = self.deck.remove_card_by_value(data)
+        self.assertEqual(removed_card, data)
         self.assertEqual(len(self.deck.cards), 51)
         self.assertFalse(data in self.deck.cards)
-    
-    def test_remove_card_invalid(self):
+
+    def test_remove_card_by_value_invalid(self):
         """
         Check when there is enough cards to be removed from the deck
         """
         self.deck.deal_cards(Hand('Test'), len(self.deck.cards))
         self.assertEqual(len(self.deck.cards), 0)
-        self.deck.remove_card(Card('8', 'Spades'))
+        self.deck.remove_card_by_value(Card('8', 'Spades'))
         self.assertEqual(len(self.deck.cards), 0)
 
     def test_deal_cards_right_number_in_deck(self):
@@ -127,7 +130,7 @@ class TestFrenchDeck(unittest.TestCase):
         Check that the right card was dealt into hand
         """
         data = Hand(player_name='testing')
-        card = self.deck.cards[len(self.deck.cards)-1]
+        card = self.deck.cards[len(self.deck.cards) - 1]
         self.deck.deal_cards(data, 1)
         self.assertEqual(data.cards[0], card)
 
@@ -136,14 +139,15 @@ class TestFrenchDeck(unittest.TestCase):
         Check when there is enough cards to be removed from the deck
         """
         data = 7
-        self.assertTrue(self.deck.check_enough_cards_in_deck(data))
+        self.assertTrue(self.deck.check_enough_cards(data))
 
     def test_check_enough_cards_in_deck_returnFalse(self):
         """
         Check when there is not enough cards to be removed from the deck
         """
         data = 56
-        self.assertFalse(self.deck.check_enough_cards_in_deck(data))
-        
+        self.assertFalse(self.deck.check_enough_cards(data))
+
+
 if __name__ == '__main__':
     unittest.main()
