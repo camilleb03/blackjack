b2cceaf88d6dbe7973ea40738393dbaa19a19c60
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 01:39:38 -0500

    Reformated CardCollection and removed specialized methods

--- playing_cards/cards_collection.py
+++ playing_cards/cards_collection.py
@@ -1,31 +1,33 @@
 import random
+
 from playing_cards import Card
+
 
 class CardsCollection:
     """
-    Represents a collection of cards
+    Represents a collection of cards (Deck, Hand, DiscardPile, etc.)
     """
 
     def __init__(self):
         self.cards = []
-    
+
     def __str__(self):
         res = []
         for card in self.cards:
             res.append(str(card))
-        return  '\n'.join(res)
-    
+        return '\n'.join(res)
+
     def get_nb_cards(self):
         return len(self.cards)
 
     def check_enough_cards(self, nb_to_remove=1):
-        return get_nb_cards() >= nb_to_remove
+        return self.get_nb_cards() >= nb_to_remove
 
     # List starts at 0
     def add_card(self, card: Card, index=None):
         if index is None:
             self.cards.append(card)
-        elif 0 <= index and index <= self.get_nb_cards():
+        elif 0 <= index <= self.get_nb_cards():
             self.cards.insert(index, card)
         else:
             print("Cannot add card")
@@ -45,35 +47,11 @@ class CardsCollection:
                 return self.cards.pop(index)
             else:
                 print("Cannot remove card by index")
-    
+
+    # TODO: Implement Fischer-Yates method ?
+    def shuffle(self):
+        random.shuffle(self.cards)
+
     @staticmethod
-    def merge_collections_cards(self, c1: CardsCollection, c2: CardsCollection):
-        return c1 + c2
-
-    # TODO: Hand class
-    def discard_cards(self, nb_to_discard):
-        if self.check_enough_cards(nb_to_discard):
-            nb_to_keep = self.get_nb_cards() - nb_to_discard
-            # Chooose randomly cards to keep
-            cards_to_keep = set(random.sample(self.cards, nb_to_keep))
-            # Rebuild collection of cards with chosen cards
-            self.cards = [card for card in self.cards if card in cards_to_keep]
-            discarded_cards = [card for card in self.cards if card not in cards_to_keep]
-            return discarded_cards
-        else:
-            print("Cannot discards cards")
-
-    # TODO: Deck class
-    def deal_cards(self, hand, nb_to_deal):
-        if self.check_enough_cards(nb_to_deal):
-            for i in range(nb_to_deal):
-                # Adds card to the end of hand
-                nb_cards_in_hand = hand.get_nb_cards()
-                hand.add_card(card=self.cards.pop())
-        # TODO: Do something when there is not enough cards to deal
-        else:
-            print("Cannot deal cards")
-            
-    # TODO: Deck class
-    def build(self):
-        raise NotImplementedError("A deck needs cards in it")
+    def merge_collections_cards(cards_from_c1: list, cards_from_c2: list):
+        return cards_from_c1 + cards_from_c2
