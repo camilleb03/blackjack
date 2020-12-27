440f6b00ad1db8f389171ead893ca2e1a2e05c57
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 17:53:50 -0500

    Developped base class CardGame

--- games/card_game/card_game.py
+++ games/card_game/card_game.py
@@ -2,4 +2,25 @@ class CardGame:
     """
     Represents a card game
     """
-    pass
+    
+    MAX_PLAYERS = None
+    GAME_NAME = None
+    deck = None
+
+    def __init__(self):
+        self.players = []
+        if self.MAX_PLAYERS == None:
+            raise NotImplementedError('Subclasses of CarGame must define MAX_PLAYERS')
+        if self.GAME_NAME == None:
+            raise NotImplementedError('Subclasses of CarGame must define GAME_NAME')
+        if self.deck == None:
+            raise NotImplementedError('Subclasses of CarGame must define deck')
+    
+    def add_players(self, players):
+        if len(self.players) + len(players) <= self.MAX_PLAYERS:
+            self.players.extend(players)
+        else:
+            print(f"Error: Max of {self.MAX_PLAYERS} for {self.GAME_NAME}.")
+
+    def show_game_title(self):
+        print(f"GAME - {self.GAME_NAME}")
--- games/war_game/war.py
+++ games/war_game/war.py
@@ -1,6 +1,13 @@
-class War():
+from games.card_game import CardGame
+from playing_cards import FrenchDeck
+
+class War(CardGame):
     """
     Represents a war game
     Reference : https://bicyclecards.com/how-to-play/war/
     """
-    pass
+    def __init__(self, nb_decks):
+        self.GAME_NAME = "War"
+        self.MAX_PLAYERS = 2
+        self.deck = FrenchDeck(nb_decks)
+        super().__init__()
