a67d511333a96760b887c1d7f8a36de3413f4482
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 01:41:06 -0500

    Modified new class DiscardPile

--- playing_cards/discard_pile.py
+++ playing_cards/discard_pile.py
@@ -1,15 +1,19 @@
-from .collection_cards import CardsCollection
+from .cards_collection import CardsCollection
+
 
 class DiscardPile(CardsCollection):
     """
     Represents a pile of discarded cards
     """
+
     def __init__(self):
-        CardsCollection.__init__(self)
-    
-    def get_top_card(self):
+        super().__init__()
+
+    def __str__(self):
+        return f"Discard pile :\n" + super().__str__()
+
+    def pop_card(self):
         if self.cards:
-            return self.cards[self.get_nb_cards() - 1]
+            return self.cards.pop()
         else:
             return None
-    
