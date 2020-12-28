23563eac27490bcc01b86e95ab9fa454a18b38d0
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 19:51:37 -0500

    Decided to not use Player Class yet

--- games/card_game/card_game.py
+++ games/card_game/card_game.py
@@ -8,7 +8,8 @@ class CardGame:
     deck = None
 
     def __init__(self):
-        self.players = []
+        self.player_names = []
+        self.hands = []
         if self.MAX_PLAYERS == None:
             raise NotImplementedError('Subclasses of CarGame must define MAX_PLAYERS')
         if self.GAME_NAME == None:
@@ -17,10 +18,13 @@ class CardGame:
             raise NotImplementedError('Subclasses of CarGame must define deck')
     
     def add_players(self, players):
-        if len(self.players) + len(players) <= self.MAX_PLAYERS:
-            self.players.extend(players)
+        if len(self.player_names) + len(players) <= self.MAX_PLAYERS:
+            self.player_names.extend(players)
         else:
             print(f"Error: Max of {self.MAX_PLAYERS} for {self.GAME_NAME}.")
+
+    def reset_players(self):
+        self.player_names = []
 
     def show_game_title(self):
         print(f"GAME - {self.GAME_NAME}")
