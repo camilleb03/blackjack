1f433911fa4fd640304f37f276e4c6054c5c56ea
Camille Bourbonnais <camille.bourbonnais@gmail.com>
2020-12-27 19:53:17 -0500

    Added BlackJackPlayer code into Player class to hopefully get rid of former

--- games/card_game/player.py
+++ games/card_game/player.py
@@ -3,9 +3,13 @@ class Player:
     Represents a player in a card game
     """
 
-    def __init__(self, name):
+    def __init__(self, name, money=1000):
         self.name = name
         self.hand = None
+        self.money = money
 
     def set_hand(self, hand):
         self.hand = hand
+    
+    def place_bet(self):
+        pass
